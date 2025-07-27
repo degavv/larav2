b8823dfb553f0bf5be818c9f30c85487a7d7b805
Vadym Dehtiarenko <degavv@gmail.com>
2025-07-27 21:20:38 +0300

    Photo model method user added

--- app/app/Models/Photo.php
+++ app/app/Models/Photo.php
@@ -2,9 +2,16 @@
 
 namespace App\Models;
 
+use App\User;
 use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
-    //
+    protected $fillable = [
+        'name',
+    ];
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
