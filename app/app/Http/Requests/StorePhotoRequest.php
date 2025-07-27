03626baf0120c63ecb6f56d793c67a5831aa5ba7
Vadym Dehtiarenko <degavv@gmail.com>
2025-07-27 21:24:24 +0300

    StorePhotoRequest refactor

--- app/app/Http/Requests/StorePhotoRequest.php
+++ app/app/Http/Requests/StorePhotoRequest.php
@@ -24,7 +24,7 @@ class StorePhotoRequest extends Request
     public function rules()
     {
         return [
-            'file' => 'required|mimes:jpeg,png,jpg|max:2048',
+            'file' => 'required|mimes:jpeg,png|max:2048',
         ];
     }
 }
