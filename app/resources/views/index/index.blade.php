a0795a7f29b45259adbdc2a5a9157919dd0bd974
dega <degavv@gmail.com>
2025-07-28 16:56:28 +0300

    index.blade changed

--- app/resources/views/index/index.blade.php
+++ app/resources/views/index/index.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
     <div class="container">
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
         @if (!Auth::guest())
             <div class="panel-body">
                 <a href="{{ route('create') }}" class="btn btn-success">Upload photo</a>
@@ -15,9 +20,22 @@
 
             <div class="panel-body">
                 @foreach ($photos as $photo)
-                    <p>{{ public_path('storage/photos/' . $photo->filename) }}</p>
-
-                    <img src="{{ asset('storage/photos/' . $photo->filename) }}" alt="{{ $photo->name }}">
+                    <div class="container">
+                        <img style="max-width:80%" src="{{ asset('photos/' . $photo->name) }}" alt="{{ $photo->name }}">
+                        <p>author: {{ $photo->user->name }}</p>
+                        @if ($photo->user->id === Auth::user()->id)
+                            <form action="{{ route('destroy', $photo->id) }}" method="post" class="form-inline pull-left"
+                                style="margin-right: 5px;">
+                                {{ csrf_field() }}
+                                {{ method_field('DELETE') }}
+                                <button class="btn btn-danger"><i class="fa fa-trash"></i></button>
+                            </form>
+                            <a class="btn btn-warning pull-left" href="{{ route('edit', $photo->id) }}">
+                                <i class="fa fa-edit"></i>
+                            </a>
+                            <div class="clearfix"></div>
+                        @endif
+                    </div>
                 @endforeach
             </div>
         </div>
